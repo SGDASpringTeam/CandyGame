f1e451d2b357fe9ec25fc7cacd676bdc105d10dc
Josh <spamalamagama@gmail.com>
2024-03-14 02:50:47 -0500

    Color Palettes Persist
    Manager now ensures color palette selection stays across scenes

--- Assets/Scripts/PaletteSwapLookup.cs
+++ Assets/Scripts/PaletteSwapLookup.cs
@@ -2,45 +2,46 @@
 using System.Collections;
 
 [ExecuteInEditMode]
-public class PaletteSwapLookup : MonoBehaviour
-{
-	public int currentPalette = 0;
-	public Texture[] LookupTextures;
+public class PaletteSwapLookup : MonoBehaviour {
+    private int MyCurrentPalette;
+    public Texture[] LookupTextures;
+    public static PaletteSwapLookup instance;
 
-	Material _mat;
+    Material _mat;
 
-	void OnEnable()
-	{
-		Shader shader = Shader.Find("Hidden/PaletteSwapLookup");
-		if (_mat == null)
-			_mat = new Material(shader);
+    void OnEnable() {
+        Shader shader = Shader.Find("Hidden/PaletteSwapLookup");
+        if (_mat == null)
+        {
+            _mat = new Material(shader);
+        }
+
+        MyCurrentPalette = Manager.currentPalette;
     }
 
-	void OnDisable()
-	{
-		if (_mat != null)
-			DestroyImmediate(_mat);
-	}
+    void OnDisable() {
+        if (_mat != null)
+            DestroyImmediate(_mat);
+    }
 
-	void OnRenderImage(RenderTexture src, RenderTexture dst)
-	{
-		_mat.SetTexture("_PaletteTex", LookupTextures[currentPalette]);
-		Graphics.Blit(src, dst,  _mat);
-	}
+    void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        _mat.SetTexture("_PaletteTex", LookupTextures[Manager.currentPalette]);
+        Graphics.Blit(src, dst, _mat);
+    }
 
-	public void NextPalette() {
-		if (currentPalette == LookupTextures.Length - 1) {
-			currentPalette = 0;
-		} else {
-			currentPalette++;
-		}
-	}
+    public void NextPalette() {
+        if (Manager.currentPalette == LookupTextures.Length - 1) {
+            Manager.currentPalette = 0;
+        } else {
+            Manager.currentPalette++;
+        }
+    }
 
-	public void PreviousPalette() {
-		if (currentPalette == 0) {
-			currentPalette = LookupTextures.Length - 1;
-		} else {
-			currentPalette--;
-		}
-	}
+    public void PreviousPalette() {
+        if (Manager.currentPalette == 0) {
+            Manager.currentPalette = LookupTextures.Length - 1;
+        } else {
+            Manager.currentPalette--;
+        }
+    }
 }
